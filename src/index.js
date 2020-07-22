6ad5bca57dde42d699d905e15c83a5c24b4abebd
Damian Duszyński <dd.duszynski@gmail.com>
2020-07-22 21:18:17 +0200

    lesson 279

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer);
+
+const app = (
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
+serviceWorker.unregister();
