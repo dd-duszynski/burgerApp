748854446cd4a1009989810c98e8645da46bf3b7
Damian Duszyński <dd.duszynski@gmail.com>
2020-07-28 07:57:29 +0200

    lesson 328 - Storing The Token

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css';
 import * as actions from '../../store/actions/index';
 
@@ -41,19 +42,6 @@ class Auth extends Component {
 		isSignup: true,
 	};
 
-	inputChangedHandler = (event, controlName) => {
-		const updatedControls = {
-			...this.state.controls,
-			[controlName]: {
-				...this.state.controls[controlName],
-				value: event.target.value,
-				valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
-				touched: true,
-			},
-		};
-		this.setState({ controls: updatedControls });
-	};
-
 	checkValidity(value, rules) {
 		let isValid = true;
 		if (!rules) {
@@ -70,17 +58,31 @@ class Auth extends Component {
 		}
 		return isValid;
 	}
+
+	inputChangedHandler = (event, controlName) => {
+		const updatedControls = {
+			...this.state.controls,
+			[controlName]: {
+				...this.state.controls[controlName],
+				value: event.target.value,
+				valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+				touched: true,
+			},
+		};
+		this.setState({ controls: updatedControls });
+	};
+
 	submitHandler = (event) => {
 		event.preventDefault();
 		this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
 	};
+
 	switchAuthModeHandler = () => {
 		this.setState((prevState) => {
-			return {
-				isSignup: !prevState.isSignup,
-			};
+			return { isSignup: !prevState.isSignup };
 		});
 	};
+
 	render() {
 		const formElementsArray = [];
 		for (let key in this.state.controls) {
@@ -89,7 +91,8 @@ class Auth extends Component {
 				config: this.state.controls[key],
 			});
 		}
-		const form = formElementsArray.map((formElement) => (
+
+		let form = formElementsArray.map((formElement) => (
 			<Input
 				key={formElement.id}
 				elementType={formElement.config.elementType}
@@ -101,6 +104,11 @@ class Auth extends Component {
 				changed={(event) => this.inputChangedHandler(event, formElement.id)}
 			/>
 		));
+
+		if (this.props.loading) {
+			form = <Spinner />;
+		}
+
 		return (
 			<div className={classes.Auth}>
 				<form onSubmit={this.submitHandler}>
@@ -108,12 +116,17 @@ class Auth extends Component {
 					<Button btnType='Success'>SUBMIT</Button>
 				</form>
 				<Button clicked={this.switchAuthModeHandler} btnType='Danger'>
-					SWITCH TO {this.state.isSignup ? 'SIGNIN' : 'SIGNUP'}{' '}
+					SWITCH TO {this.state.isSignup ? 'SIGN IN' : 'SIGN UP'}
 				</Button>
 			</div>
 		);
 	}
 }
+const mapStateToProps = (state) => {
+	return {
+		loading: state.auth.loading,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -121,4 +134,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,16 +1,18 @@
 //https://firebase.google.com/docs/reference/rest/auth?authuser=0#section-create-email-password <-- dokumentacja
 import axios from 'axios';
 import * as actionTypes from './actionTypes';
+
 export const authStart = () => {
 	return {
 		type: actionTypes.AUTH_START,
 	};
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
 	return {
 		type: actionTypes.AUTH_SUCCESS,
-		authData: authData,
+      idToken: token,
+      userId: userId
 	};
 };
 
@@ -22,29 +24,25 @@ export const authFail = (error) => {
 };
 
 export const auth = (email, password, isSignup) => {
-	console.log('[isSignup]', isSignup);
-
-	return (dispatch) => {
-		dispatch(authStart());
-		const authData = {
-			email: email,
-			password: password,
-			returnSecureToken: true,
-		};
-		let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs2SM9hdOR6Q45ptULLFTWL6r-vOtW5Xw';
-		if (!isSignup) {
-			url =
-				'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBs2SM9hdOR6Q45ptULLFTWL6r-vOtW5Xw';
-		}
-		axios
-			.post(url, authData)
-			.then((response) => {
-				console.log(response);
-				dispatch(authSuccess(response.data));
-			})
-			.catch((err) => {
-				console.log(err);
-				dispatch(authFail(err));
-			});
-	};
+   return dispatch => {
+       dispatch(authStart());
+       const authData = {
+           email: email,
+           password: password,
+           returnSecureToken: true
+       };
+       let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBs2SM9hdOR6Q45ptULLFTWL6r-vOtW5Xw';
+       if (!isSignup) {
+           url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBs2SM9hdOR6Q45ptULLFTWL6r-vOtW5Xw';
+       }
+       axios.post(url, authData)
+           .then(response => {
+               console.log(response);
+               dispatch(authSuccess(response.data.idToken, response.data.localId));
+           })
+           .catch(err => {
+               console.log(err);
+               dispatch(authFail(err));
+           });
+   };
 };
